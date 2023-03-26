a7ee757ca28864ec8222f66eabe69acdf1a56980
dido29 <127900393+dido29@users.noreply.github.com>
2023-03-26 01:57:58 +0100

    Update index.php

--- e-commerce/index.php
+++ e-commerce/index.php
@@ -11,6 +11,7 @@
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css">
+            <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
             <style>
             .font{
